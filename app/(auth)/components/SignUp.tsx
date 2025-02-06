a7f1a41e9fbb1e838b2c147b6877784bbbf2c6fb
najwang <anyonanicholas86@gmail.com>
2025-02-06 21:04:01 +0300

    feat: make name optional in sign ups

--- app/(auth)/components/SignUp.tsx
+++ app/(auth)/components/SignUp.tsx
@@ -9,11 +9,11 @@ import FormControl from '@mui/material/FormControl';
 import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import {FacebookIcon, GoogleIcon} from './CustomIcons';
-import {FaviconRow as SitemarkIcon} from '@/components/internal/icons/Favicon';
-import {MtCard} from "@/components/internal/styled/MtCard";
-import {paths} from "@/lib/paths";
-import {createClient} from "@/utils/supabase/client";
+import { FacebookIcon, GoogleIcon } from './CustomIcons';
+import { FaviconRow as SitemarkIcon } from '@/components/internal/icons/Favicon';
+import { MtCard } from "@/components/internal/styled/MtCard";
+import { paths } from "@/lib/paths";
+import { createClient } from "@/utils/supabase/client";
 import { useRouter } from 'next/navigation';
 
 const supabase = createClient();
@@ -21,27 +21,29 @@ const supabase = createClient();
 type SignUpProps = {
     // A dispatcher, that when set to true, will display a dialogue,
     // telling the user the given action is under construction
-    setUnderConstruction:  React.Dispatch<React.SetStateAction<boolean>>,
-    setSubmissionError:  React.Dispatch<React.SetStateAction<boolean>>,
-    setSubmissionMessage:  React.Dispatch<React.SetStateAction<string>>,
-    setShowEmailConfirmation:  React.Dispatch<React.SetStateAction<boolean>>,
+    setUnderConstruction: React.Dispatch<React.SetStateAction<boolean>>,
+    setSubmissionError: React.Dispatch<React.SetStateAction<boolean>>,
+    setSubmissionMessage: React.Dispatch<React.SetStateAction<string>>,
+    setShowEmailConfirmation: React.Dispatch<React.SetStateAction<boolean>>,
 }
 
 export default function SignUp(props: SignUpProps) {
-    const {setUnderConstruction, setSubmissionMessage, setSubmissionError, setShowEmailConfirmation} = props;
+    const { setUnderConstruction, setSubmissionMessage, setSubmissionError, setShowEmailConfirmation } = props;
     const [emailError, setEmailError] = React.useState(false);
     const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
     const [passwordError, setPasswordError] = React.useState(false);
     const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
-    const [nameError, setNameError] = React.useState(false);
-    const [nameErrorMessage, setNameErrorMessage] = React.useState('');
+    const [checked, setChecked] = React.useState(true);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked);
+    };
 
     const router = useRouter();
 
     const validateInputs = () => {
         const email = document.getElementById('email') as HTMLInputElement;
         const password = document.getElementById('password') as HTMLInputElement;
-        const name = document.getElementById('name') as HTMLInputElement;
 
         let isValid = true;
 
@@ -63,34 +65,25 @@ export default function SignUp(props: SignUpProps) {
             setPasswordErrorMessage('');
         }
 
-        if (!name.value || name.value.length < 1) {
-            setNameError(true);
-            setNameErrorMessage('Name is required.');
-            isValid = false;
-        } else {
-            setNameError(false);
-            setNameErrorMessage('');
-        }
-
         return isValid;
     };
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if (nameError || emailError || passwordError) {
+
+        if (emailError || passwordError) {
             return;
         }
 
         const formData = new FormData(event.currentTarget);
-        const {name, email, password} = {
-            name: formData.get('name'),
+        const { email, password } = {
             email: formData.get('email'),
             password: formData.get('password'),
         }
 
-        if (!name || !email || !password) {
+        if (!email || !password) {
             setSubmissionError(true);
-            setSubmissionMessage("Invalid Credentials");
+            setSubmissionMessage("Enter Credentials");
             return;
         }
 
@@ -100,12 +93,14 @@ export default function SignUp(props: SignUpProps) {
             redirectTo = undefined;
         }
 
-        console.log("Sending SignUp request: email:", email, " password: ", password);
         const { data, error } = await supabase.auth.signUp({
             email: email as string,
             password: password as string,
             options: {
                 emailRedirectTo: redirectTo,
+                data: {
+                    subscribe: checked,
+                },
             },
         });
         console.log("SignUp response: ", error);
@@ -118,11 +113,11 @@ export default function SignUp(props: SignUpProps) {
         }
 
         if (data.user) {
-            if(data.user.email_confirmed_at){
+            if (data.user.email_confirmed_at) {
                 // This user is already a member, redirect to login page
                 let goTo: string = paths.auth.signIn;
-                if (redirectTo){
-                    goTo = `${goTo}/redirect_to=${redirectTo}`;
+                if (redirectTo) {
+                    goTo = `${goTo}/?redirect_to=${redirectTo}`;
                 }
                 router.replace(goTo);
                 return;
@@ -134,33 +129,19 @@ export default function SignUp(props: SignUpProps) {
 
     return (
         <MtCard variant="outlined">
-            <SitemarkIcon/>
+            <SitemarkIcon />
             <Typography
                 component="h1"
                 variant="h4"
-                sx={{width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)'}}
+                sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
             >
                 Sign up
             </Typography>
             <Box
                 component="form"
                 onSubmit={handleSubmit}
-                sx={{display: 'flex', flexDirection: 'column', gap: 2}}
+                sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
             >
-                <FormControl>
-                    <FormLabel htmlFor="name">Full name</FormLabel>
-                    <TextField
-                        autoComplete="name"
-                        name="name"
-                        required
-                        fullWidth
-                        id="name"
-                        placeholder="Jon Doe Snow"
-                        error={nameError}
-                        helperText={nameErrorMessage}
-                        color={nameError ? 'error' : 'primary'}
-                    />
-                </FormControl>
                 <FormControl>
                     <FormLabel htmlFor="email">Email</FormLabel>
                     <TextField
@@ -193,7 +174,13 @@ export default function SignUp(props: SignUpProps) {
                     />
                 </FormControl>
                 <FormControlLabel
-                    control={<Checkbox value="allowExtraEmails" color="primary"/>}
+                    control={
+                        <Checkbox
+                            checked={checked}
+                            onChange={handleChange}
+                            inputProps={{ 'aria-label': 'controlled' }}
+                            color='primary' />
+                    }
                     label="I want to receive updates via email."
                 />
                 <Button
@@ -204,26 +191,26 @@ export default function SignUp(props: SignUpProps) {
                 >
                     Sign up
                 </Button>
-                <Typography sx={{textAlign: 'center'}}>
+                <Typography sx={{ textAlign: 'center' }}>
                     Already have an account?{' '}
                     <Link
                         href={paths.auth.signIn}
                         variant="body2"
-                        sx={{alignSelf: 'center'}}
+                        sx={{ alignSelf: 'center' }}
                     >
                         Sign in
                     </Link>
                 </Typography>
             </Box>
             <Divider>
-                <Typography sx={{color: 'text.secondary'}}>or</Typography>
+                <Typography sx={{ color: 'text.secondary' }}>or</Typography>
             </Divider>
-            <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
                 <Button
                     fullWidth
                     variant="outlined"
                     onClick={() => setUnderConstruction(true)}
-                    startIcon={<GoogleIcon/>}
+                    startIcon={<GoogleIcon />}
                 >
                     Sign up with Google
                 </Button>
@@ -231,7 +218,7 @@ export default function SignUp(props: SignUpProps) {
                     fullWidth
                     variant="outlined"
                     onClick={() => setUnderConstruction(true)}
-                    startIcon={<FacebookIcon/>}
+                    startIcon={<FacebookIcon />}
                 >
                     Sign up with Facebook
                 </Button>
